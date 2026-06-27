479dd1946bc910f953d38eaf495083844488e796
renishandrick <renishandrick@gmail.com>
2026-06-27 17:16:08 +0530

    complete basic backend

--- BackendDemo/src/main/java/com/Backend/BackendDemo/trackerController.java
+++ BackendDemo/src/main/java/com/Backend/BackendDemo/trackerController.java
@@ -1,18 +1,37 @@
 package com.Backend.BackendDemo;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import com.Backend.BackendDemo.entites.tracker;
+
+import java.util.List;
+
 
 @RestController
 @RequestMapping("/track")
 public class trackerController {
     @Autowired
     private trackerService ts;
-    @GetMapping("/get")
-    String tracker(){
-
-        return "tracking";
+    @PostMapping("/create")
+    ResponseEntity createUser (@RequestBody tracker t){
+        return new ResponseEntity<>(ts.createTracker(t), HttpStatus.CREATED);
+    }
+    @GetMapping("/all")
+    public List<tracker> getAll(){
+        return ts.getAllTrackers();
+    }
+    @GetMapping("/{id}")
+    public tracker getById(@PathVariable int id){
+        return ts.getTracker(id);
+    }
+    @PutMapping("/update")
+    public tracker update(@PathVariable tracker t){
+        return ts.updateTracker(t);
+    }
+    @DeleteMapping("/delete/{id}")
+    public String delete(@PathVariable int id){
+        return ts.deleteTracker(id);
     }
 }
--- BackendDemo/src/main/java/com/Backend/BackendDemo/trackerService.java
+++ BackendDemo/src/main/java/com/Backend/BackendDemo/trackerService.java
@@ -2,6 +2,9 @@ package com.Backend.BackendDemo;
 import com.Backend.BackendDemo.entites.tracker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
 
 @Service
 public class trackerService {
@@ -9,5 +12,18 @@ public class trackerService {
     private trackerRepository tr;
     public tracker createTracker(tracker t){
         return tr.save(t);
+    }
+    public List<tracker> getAllTrackers(){
+        return tr.findAll();
+    }
+    public tracker getTracker(int id){
+        return tr.findById(id).orElse(null);
+    }
+    public tracker updateTracker(tracker t){
+        return tr.save(t);
+    }
+    public String deleteTracker(int id){
+        tr.deleteById(id);
+        return "Deleted successfully";
     }
 }
